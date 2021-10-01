747c6473b87514c1fab6d80505ae9a382d2250c8
gui-back <guilherme.oliveira2018@outlook.com.br>
2021-10-01 14:38:49 -0300

    Criado e removido atributo dinâmicode objetos do tipo Pessoa

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,6 +1,5 @@
 class Pessoa:
     def __init__(self, *filhos, nome = None, idade= 35):
-
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
@@ -19,6 +18,12 @@ if __name__ == '__main__':
     print(luciano.cumprimentar())
     print(luciano.nome)
     print(luciano.idade)
-
     for filhos in luciano.filhos:
         print(filhos.nome)
+
+    luciano.sobrenome = 'Ramalho'
+    del luciano.filhos
+
+    print(luciano.__dict__)
+    print(renzo.__dict__)
+
